559e8dd050b5d120ab486babab96a6b537956fb6
aeksei <aeksei94@gmail.com>
2023-06-19 17:04:13 +0300

    init

---
+++ Введение в программирование на python/Основные элементы программирования/Ввод значения переменной во время работы программы/main.py
@@ -0,0 +1,4 @@
+name = ...  # input("Введите имя:")
+print("Hello,", name)
+
+print(type(name))  # type позволяет получить тип переменной
---
+++ Введение в программирование на python/Основные элементы программирования/Как желательно называть переменные/task.py
@@ -0,0 +1,11 @@
+SECONDS_IN_MINUTE = 60  # количество секунд в минуте
+SECONDS_IN_HOUR = 60 * 60  # количество секунд в часе
+SECONDS_IN_DAY = 24 * 60 * 60  # количество секунд в дне
+
+total_seconds_in_two_minutes = 2 * SECONDS_IN_MINUTE  # количество секунд в двух минутах
+total_seconds_in_five_hours = 5 * SECONDS_IN_HOUR  # количество секунд в пяти часах
+total_seconds_in_ten_days = 10 * SECONDS_IN_DAY  # количество секунд в десяти днях
+
+print("Количество секунд в двух минутах:", total_seconds_in_two_minutes)
+print("Количество секунд в пяти часах:", total_seconds_in_five_hours)
+print("Количество секунд в десяти днях:", total_seconds_in_ten_days)
